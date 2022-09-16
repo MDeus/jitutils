c3dfae5d3c06a5baa1ccab1914c25efa396350ff
Maudeline Deus <maudelinedeus@gmail.com>
2022-09-16 00:24:04 -0400

    takes input and returns multiple dot files if needed

--- OSSE_proj/Graph.cs
+++ OSSE_proj/Graph.cs
@@ -1,16 +1,14 @@
 class Vertex {
     private String label;
     private List<String> content_code;
-    private int size;
     private double weight;
     private double score;
 
-    public Vertex (String label, int size, double weight, double score, List<String> content) {
+    public Vertex (String label, double weight, double score, List<String> content) {
         this.label = label;
         this.content_code = content;
         this.weight = weight;
         this.score = score;
-        this.size = size;
     }
 
     public Boolean Equals (Vertex v1){
@@ -21,12 +19,8 @@ class Vertex {
         this.content_code = content;
     }
 
-    public void setSize (int size) {
-        this.size = size;
-    }
-
-    public int getSize () {
-        return size;
+    public List<String> getContentCode () {
+        return content_code;
     }
 
     public void setWeight (int weight) {
@@ -49,22 +43,25 @@ class Vertex {
         return label;
     }
 
-    public void print() {
-        // Console.WriteLine("\t{0} [label = \"{1}\\n\\n{2}\" style=\"solid\" penwidth={3}];",label, label, string.Join("\\l", content_code), weight);
+    // public void print() {
+  
+    //     Console.WriteLine("\t{0} [label =<", label);
+    //     Console.WriteLine("\t\t<table border=\"0\" cellborder=\"0\" cellspacing=\"1\">");
+    //     Console.WriteLine("\t\t<tr><td align=\"center\"><b>{0}</b></td></tr>", label);
 
-        Console.WriteLine("\t{0} [label =<", label);
-        Console.WriteLine("\t\t<table border=\"0\" cellborder=\"0\" cellspacing=\"1\">");
-        Console.WriteLine("\t\t<tr><td align=\"center\"><b>{0}</b></td></tr>", label);
-
-        foreach(var code in content_code) {
-            Console.WriteLine("\t\t<tr><td align=\"left\">{0}</td></tr>", code);
-        }
-        Console.WriteLine("\t\t</table>> style=\"solid\" penwidth={0} ];", 1);
-    }
+    //     foreach(var code in content_code) {
+    //         Console.WriteLine("\t\t<tr><td align=\"left\">{0}</td></tr>", code);
+    //     }
+    //     Console.WriteLine("\t\t</table>> style=\"solid\" penwidth={0} ];", 1);
+    // }
 }
 
 class Graph {
     private Dictionary<Vertex, List<Vertex>> adjVertices;
+
+    private double maxWeight = 0;
+
+    private double maxScore = 0;
 
     public Graph () {
         this.adjVertices = new Dictionary<Vertex, List<Vertex>>();
@@ -76,6 +73,22 @@ class Graph {
 
     public void AddEdge (Vertex v1, Vertex v2) {
         adjVertices[v1].Add(v2);
+    }
+
+    public void setMaxWeight (double weight) {
+        this.maxWeight = weight;
+    }
+
+    public double getMaxWeight () {
+        return maxWeight;
+    }
+
+    public void setMaxScore (double score) {
+        this.maxScore = score;
+    }
+
+    public double getMaxScore () {
+        return maxScore;
     }
 
     public List<Vertex> getVertices() {
@@ -94,36 +107,68 @@ class Graph {
         throw new InvalidDataException("This vertex does not exist");
     }
 
-    public void print () {
-        Console.WriteLine("digraph FlowGraph {");
-        Console.WriteLine("\tsplines=fase;");
-        Console.WriteLine("\tgraph [label = \"Bench()\"];");
-        Console.WriteLine("\tnode [shape = \"Box\" fontname=\"Courier New\"];");
+    private int choose_color (double score) {
+        if (score < 0.2) {return 1;}
+        else if (score >= 0.2 && score < 0.4) {return 2;}
+        else if (score >= 0.4 && score < 0.6) {return 3;}
+        else if (score >= 0.6 && score < 0.8) {return 4;}
+        else  {return 5;}
 
-        foreach (var pair in adjVertices) {
-            Vertex v = pair.Key;
-            v.print();
-        }
+    }
 
-        Console.WriteLine("\n");
+    public void print (String file_name) {
+        using(StreamWriter writetext = new StreamWriter(file_name)) {
 
-        foreach (var pair in adjVertices) {
-            Vertex v = pair.Key;
+            writetext.WriteLine("digraph FlowGraph {");
+            writetext.WriteLine("\tsplines=fase;");
+            writetext.WriteLine("\tgraph [label = \"Bench()\"];");
+            writetext.WriteLine("\tnode [shape = \"Box\" fontname=\"Courier New\" colorscheme=oranges9];");
 
-            foreach (var vi in pair.Value) {
-                if (vi.Equals(v)) {
-                    Console.WriteLine("\t{0} -> {1} [color=red, dir=back];", v.getLabel(), vi.getLabel());
-                } else {
-                    if (String.Compare(v.getLabel(), vi.getLabel()) > 0 ) {
-                        Console.WriteLine("\t{0} -> {1} [color=green];", v.getLabel(), vi.getLabel());    
-                    }
-                    else {
-                        Console.WriteLine("\t{0} -> {1} [color=blue];", v.getLabel(), vi.getLabel());
+            foreach (var pair in adjVertices) {
+                Vertex v = pair.Key;
+
+                writetext.WriteLine("\t{0} [label =<", v.getLabel());
+                writetext.WriteLine("\t\t<table border=\"0\" cellborder=\"0\" cellspacing=\"1\">");
+                writetext.WriteLine("\t\t<tr><td align=\"center\"><b>{0}</b></td></tr>", v.getLabel());
+
+                foreach(var code in v.getContentCode()) {
+                    writetext.WriteLine("\t\t<tr><td align=\"left\">{0}</td></tr>", code);
+                }
+                writetext.WriteLine("\t\t</table>> style=\"solid\" penwidth={0} style=filled, fillcolor={1}];", Math.Round((v.getWeight() / maxWeight), 1 ) + 0.1, choose_color(Math.Round(v.getScore() / maxScore, 2)));
+            }
+
+            writetext.WriteLine("\n");
+
+            foreach (var pair in adjVertices) {
+                Vertex v = pair.Key;
+
+                foreach (var vi in pair.Value) {
+                    if (vi.Equals(v)) {
+                        writetext.WriteLine("\t{0} -> {1} [color=red, dir=back];", v.getLabel(), vi.getLabel());
+                    } else {
+                        if (String.Compare(v.getLabel(), vi.getLabel()) > 0 ) {
+                            writetext.WriteLine("\t{0} -> {1} [color=green];", v.getLabel(), vi.getLabel());    
+                        }
+                        else {
+                            writetext.WriteLine("\t{0} -> {1} [color=blue];", v.getLabel(), vi.getLabel());
+                        }
                     }
                 }
             }
-        }
+            
+            List<Vertex> vertices = getVertices();
+            int t_vertices = vertices.Count;
+            
+            String edge = "\tsubgraph cluster_1 {" + vertices[0].getLabel() +" -> ";
+            for (int i =1; i < t_vertices - 1; i++) {
+                edge = edge + (vertices[i].getLabel() + " -> ");
+            }
 
-        Console.WriteLine("}");        
+            writetext.WriteLine("\n\tedge[style=invis];");
+            writetext.WriteLine(edge + vertices[t_vertices - 1].getLabel() + "};");      
+
+            writetext.WriteLine( "}");   
+
+        }     
     }
 }
--- OSSE_proj/Program.cs
+++ OSSE_proj/Program.cs
@@ -1,120 +1,8 @@
 ﻿
 class Program {
-    static public void Main(String[] args) {
-        // Read a text file line by line.  
-        string[] lines = File.ReadAllLines("assembly2.txt");  
 
-        int j = -1;
-        String current_node = ""; // holds the current node
-        var nodes     = new List<String>(); 
-        var temp_conn = new HashSet<String>(); // holds all the nodes or edges that are connected to the current node
-        var temp_cont = new List<String>(); // holds code block inside the current node
-        Dictionary<String, List<String>> arrows = new Dictionary<String, List<String>>(); // dict of connected nodes and their edges
-        Dictionary<String, List<String>> node_content = new Dictionary<String, List<String>>(); // dict of nodes and the code block inside
-        Dictionary<String, String> weight_dict = new Dictionary<String, String>(); // dict of nodes and weight
-        Dictionary<String, String> score_dict = new Dictionary<String, String>(); // dict of nodes and score
-        Dictionary<String, String> size_dict = new Dictionary<String, String>(); // dict of nodes and size
-
-        // iterate over the lines of the file
-        for (int i = 0; i < lines.Length; i++) {
-            // remove leading spaces
-            String line = lines[i].Trim();
-            String[] arr = line.Split(null, 2);
-
-            // get the line excluding the "bar code" if it exists
-            if (arr.Length > 1) {
-                if (!Char.IsLower(arr[0][0])) {
-                    line = arr[1].Trim();
-                } 
-            } 
-            else {line = arr[0];}
-
-            // get the first word
-            String first_word = line.Split(null, 2)[0];
-            
-            // check that this is the start of a group
-            if (first_word.Contains("G_M")) {
-                // add new node to list
-                String node = "IG" + first_word.Split("_IG")[1];
-                node = node.Substring(0, node.Length - 1);
-                nodes.Add(node);
-
-                if (j >= 0) {
-                
-                    // add the nodes connection nodes to dict
-                    temp_conn.Add(node);
-                    arrows.Add(current_node, temp_conn.ToList());
-                    temp_conn = new HashSet<String>(); 
-
-                    // add the nodes content code to dict
-                    node_content.Add(current_node, temp_cont);
-                    temp_cont = new List<String>(); 
-
-                }
-
-                j++;
-                current_node = node;
-            }
-            // get nodes connected to current node
-            // else if (first_word[0] == 'j' || first_word.Contains("cb") || first_word == "b"||
-            //         (first_word[0] == 'b' && (first_word != "bl" && first_word != "blr" && first_word != "bkpt" && !first_word.Contains("bbWeight")))) {
-            else if (first_word[0] == 'j' || line.Contains("G_M")){ // check that its a jump condition
-
-                temp_conn.Add("IG" + line.Split("_IG")[1]);
-                temp_cont.Add(lines[i].Trim());
-            }
-            // get the weight and score of current node
-            else if (line.Contains("bbWeight=")) { // get the group stats
-
-                String weight = line.Split("bbWeight=")[1].Split(" ")[0];
-                String score = line.Split("PerfScore ")[1].Split(" ")[0];
-                String size = "0";
-                if (line.Contains("size=")) {
-                    size = line.Split("size=")[1].Split(" ")[0];
-                }
-
-                weight_dict.Add(current_node, weight);
-                score_dict.Add(current_node, score);
-                size_dict.Add(current_node, size);
-            }
-            // add everything else to content dict
-            else { 
-
-                temp_cont.Add(lines[i].Trim());
-            }
-
-            if (lines.Length == i + 1) {
-
-                // add the nodes connection nodes to dict
-                arrows.Add(current_node, temp_conn.ToList());
-                temp_conn = new HashSet<String>(); 
-
-                // add the nodes content code to dict
-                node_content.Add(current_node, temp_cont);
-                temp_cont = new List<String>(); 
-
-            }
-        }
-
-        // create graph instantiation
-        Graph graph = new Graph();
-
-        // create vertices
-        foreach (var n in nodes) {
-
-            double sc = Convert.ToDouble(score_dict[n]);
-            double w = Convert.ToDouble(weight_dict[n]);
-            int s = Convert.ToInt32(size_dict[n]);
-
-            List<String> content = node_content[n];
-
-            Vertex v = new Vertex(n, s, w, sc, content);
-
-            graph.AddVertex(v);
-        }
-
-        // add edges to graph
-        foreach (var pair in arrows) {
+    public static Graph addEdges(Graph graph, Dictionary<String, List<String>> edges) {
+        foreach (var pair in edges) {
             String label = pair.Key;
             Vertex v1;
             try {
@@ -137,8 +25,164 @@ class Program {
             }
 
         }
+        return graph;
+    }
+
+    static public void Main(String[] args) {
+        // Read a text file line by line.  
+        if (!File.Exists(args[0])) {
+            throw new InvalidDataException ("ERROR: Incorrect file path " + args[0]);
+        }
+
+        string[] lines = File.ReadAllLines(args[0]);  
+
+        int j = -1;
+        String current_node = ""; // holds the current node
+        var temp_conn = new HashSet<String>(); // holds all the nodes or edges that are connected to the current node
+        var temp_cont = new List<String>(); // holds code block inside the current node
+        Dictionary<String, List<String>> arrows = new Dictionary<String, List<String>>(); // dict of connected nodes and their edges
+        List<Graph> graph_list = new List<Graph>();
+        Graph temp_graph = new Graph();
+        double max_w = 0.0;
+        double max_s = 0.0;
+        double temp_weight = 0;
+        double temp_score = 0;
+        int g_count = 0;
+
+        Boolean connect = true;
+        Boolean keep_bar_code = false;
+
+        if (args.Length > 1) {
+            try {
+                keep_bar_code = Convert.ToBoolean(args[1]);
+            } catch (FormatException) {
+                Console.WriteLine ("ERROR: Incorrect format for boolean");
+            }
+        }
+
+
+        // iterate over the lines of the file
+        for (int i = 0; i < lines.Length; i++) {
+            if (g_count == 5) {break;}
+            // remove leading spaces
+            String line = lines[i].Trim();
+            String[] arr = line.Split(null, 2);
+            String first_word = line.Split(null, 2)[0];
+
+            if (line.CompareTo("") == 0 || (line[0].CompareTo(';') == 0 && !line.Contains("Weight"))) {continue;}
+
+            // get the line excluding the "bar code" if it exists
+            if (!keep_bar_code) {
+                if (arr.Length > 1) {
+                    if (!Char.IsLower(arr[0][0])) {
+                        line = arr[1].Trim();
+                    }
+
+                    first_word = line.Split(null, 2)[0];
+                } 
+            } else {
+                if (arr.Length > 1) {
+                    if (!Char.IsLower(arr[0][0])) {
+                        first_word = arr[1].Trim().Split(null, 2)[0];
+                    }
+                }
+            }
+
+            if (first_word.CompareTo("ret") == 0 || first_word.CompareTo("b") == 0 ||  first_word.CompareTo("jmp") == 0) {
+                connect = false;
+            }
+            
+            // check that this is the start of a group
+            if (first_word.Contains("G_M")) {
+
+                String node = "IG" + first_word.Split("_IG")[1];
+                node = node.Substring(0, node.Length - 1);
+
+                if (node.CompareTo("IG01") == 0) {
+                    
+                    if (j >= 0) {
+
+                        Vertex v = new Vertex(current_node, temp_weight, temp_score, temp_cont);
+                        temp_graph.AddVertex(v);
+                        temp_graph.setMaxWeight(max_w);
+                        temp_graph.setMaxScore(max_s);
+                        graph_list.Add(addEdges(temp_graph, arrows));
+
+                        arrows = new Dictionary<String, List<String>>();
+                        temp_graph = new Graph();
+                        temp_conn = new HashSet<String>(); 
+                        temp_cont = new List<String>(); 
+                        j = -1;
+                        max_w = 0; max_s = 0;
+
+                        g_count++;
+                    }
+
+                }
+
+                if (j >= 0) {
+
+                    // add the nodes connection nodes to dict
+                    if (connect){
+                        temp_conn.Add(node);
+                    }
+
+                    Vertex v = new Vertex(current_node, temp_weight, temp_score, temp_cont);
+                    temp_graph.AddVertex(v);
+                    
+                    // keep track of edges
+                    arrows.Add(current_node, temp_conn.ToList());
+
+                    temp_conn = new HashSet<String>(); 
+                    temp_cont = new List<String>(); 
+                    temp_weight = 0; temp_score = 0;
+
+                }
+
+                j++;
+                current_node = node;
+                connect = true;
+            }
+            // get nodes connected to current node
+            else if (line.Contains("G_M")){ // check that its a jump condition
+                String[] line_arr = line.Split("G_M");
+                String temp_node = "IG" + line_arr[1].Split("_IG")[1];
+        
+                temp_conn.Add(temp_node);
+                temp_cont.Add(line_arr[0] + temp_node);
+            }
+            // get the weight and score of current node
+            else if (line.Contains("bbWeight=")) { // get the group stats
+
+                temp_weight = Convert.ToDouble(line.Split("bbWeight=")[1].Split(" ")[0]);
+                temp_score = Convert.ToDouble(line.Split("PerfScore ")[1].Split(" ")[0]);
+
+                if (temp_weight > max_w) {max_w = temp_weight;}
+                if (temp_score > max_s) {max_s = temp_score;}
+
+            }
+            // add everything else to content dict
+            else { 
+                temp_cont.Add(line);
+            }
+
+            if (lines.Length == i + 1) {
+                Vertex v = new Vertex(current_node, temp_weight, temp_score, temp_cont);
+                temp_graph.AddVertex(v);
+
+                temp_graph.setMaxWeight(max_w);
+                temp_graph.setMaxScore(max_s);
+
+                graph_list.Add(addEdges(temp_graph, arrows));
+            }
+        }
 
         // print graphviz file
-        graph.print();
+        int z = 0;
+        foreach (var g in graph_list) {
+            Console.WriteLine(z);
+            g.print("graph"+z+".dot");
+            z++;
+        }
     }
 }
